3661eb5e8c1b30b9266fda118b3bd5f17d055fd0
Abhijith1998 <123438717+Abhijith1998@users.noreply.github.com>
2023-12-24 22:37:53 +0530

    first push

---
+++ ELF_Base_Framework/src/main/java/org/automation/generic_utilities/InitObjects.java
@@ -0,0 +1,23 @@
+package org.automation.generic_utilities;
+
+import org.openqa.selenium.WebDriver;
+
+
+public class InitObjects {
+
+	public ActionsUtil getActionsUtil(WebDriver driver) {
+		return new ActionsUtil(driver);
+	}
+	
+	public JavaScriptUtil getJavaScriptUtil(WebDriver driver) {
+		return new JavaScriptUtil(driver);
+	}
+	
+	public ReadTestData getReadTestData() {
+		return new ReadTestData();
+	}
+	
+	public UtilityMethods getUtilityMethods() {
+		return new UtilityMethods();
+	}
+}
